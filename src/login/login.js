75252b1ff334daee87134d2e80db0f4ccf1e77fc
Calvin <calvingsx@gmail.com>
2021-11-11 08:00:44 -0300

    Adding post request to perform a login authentication

--- src/login/login.js
+++ src/login/login.js
@@ -1,18 +1,37 @@
-const showAndHiddenPassword = document.querySelector('#showAndHiddenPasswordButton')
-const loginPassword = document.querySelector('#password')
+// FUNÇÃO PARA MOSTRAR E ESCONDER A SENHA
 
-console.log("Fora da promoção")
+const showAndHiddenPassword = document.getElementById(
+  'showAndHiddenPasswordButton'
+)
+const loginPassword = document.getElementById('password')
 
-
-
-
-togglePasswordShow() {
-  console.log("dentro da função")
-  if(loginPassword.type == 'password') {
+function togglePasswordView() {
+  if (loginPassword.type == 'password') {
     loginPassword.type = 'text'
     showAndHiddenPassword.textContent = 'Esconder'
   } else {
     loginPassword.type = 'password'
     showAndHiddenPassword.textContent = 'Mostrar'
   }
+}
+
+// MÉTODO POST VIA AXIOS PARA AUTENTICAÇÃO DO USUÁRIO
+
+const API_URL = 'https://tex-game-shopping.herokuapp.com/auth/gettoken'
+
+function login() {
+  event.preventDefault()
+  const username = document.getElementById('email').value
+  const userpassword = document.getElementById('password').value
+
+  const user = {
+    username: username,
+    userpassword: userpassword
+  }
+  axios
+    .post(API_URL, {
+      username: user.username,
+      password: user.userpassword
+    })
+    .then(response => console.log(response))
 }
